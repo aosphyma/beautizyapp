735c8c2ec57c94df1fab643c82a41776d59b8bf4
aosphyma <asok12@tu-clausthal.de>
2018-12-11 19:15:45 +0100

    Search method updated

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,42 @@
 var express = require('express');
+var createError = require('http-errors');
+var mysql = require('promise-mysql');
 var router = express.Router();
 
-router.post('/', function(req, res, next){
-  console.log('that post was= ', req.get('place'));
-  console.log('that post was= ', req.get('hairstyle'));
-  res.render('search', { 
-    app_title: 'Beautizy',
-    title: 'Beautizy',
-    info: 'Search for a hairstyle at any place',
-    place: req.body.place,
-    cookies: req.cookies,
-    hairstyle: req.body.hairstyle
-  });
+
+const format = /[!@#$%^&*()_+=\[\]{};:\\|.\/?]/;
+
+router.post('/', function (req, res, next) {
+    if (format.test(req.body.place) || format.test(req.body.hairstyle)) {
+        next(createError(400));
+    }
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'beautizyapp'
+    }).then(function (connection) {
+        connection.query(
+            "SELECT beautizyapp.offer.*, beautizyapp.customer.username, beautizyapp.gallery.path "+
+            "FROM beautizyapp.offer "+
+            "JOIN beautizyapp.customer "+
+            "ON beautizyapp.offer.seller_id = beautizyapp.customer.id "+
+            "JOIN beautizyapp.gallery "+
+            "ON beautizyapp.offer.id = beautizyapp.gallery.offer_id "+
+            "WHERE beautizyapp.offer.seller_id = " +
+            "(SELECT id FROM beautizyapp.customer where " +
+            // "locate(c_street, 'req.body.place') and " +
+            "locate(c_town, '" + req.body.place + "') and " +
+            "locate(c_zip, '" + req.body.place + "') and " +
+            "locate(c_country, '" + req.body.place + "'));").
+            then(function (result) {
+                res.render('search', {
+                    title: 'Beautizy - Search',
+                    cookies: req.cookies,
+                    results: result
+                });
+            });
+    });
+
 });
 module.exports = router;
